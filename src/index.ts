5c034b4f261863915e30e2f722a79a34fa2026e0
Ranviee0 <neodyte@gmail.com>
2026-07-25 12:18:51 +0700

    feat: Initial Human Commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
+import { openapi } from '@elysia/openapi'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(openapi()).get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
